a96acfaceca055492dc769075d1c3c19199b8682
RegularRabbit05 <69511985+RegularRabbit05@users.noreply.github.com>
2021-11-07 22:39:25 +0100

    First Version

---
+++ OnichanS debit card/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OnichanS_debit_card
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ OnichanS debit card/p-please senpai.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace OnichanS_debit_card
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
